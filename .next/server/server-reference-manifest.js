017d9244b3bf998369ad269ef43f8b71eca915ea
k-syou <rnjstn819@gmail.com>
2024-01-26 12:37:24 +0900

    git ignore 수정

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YAi5Ydtucbz14tLVBL9tKQgwiiQmAOKyB+ezBO+rCjQ=\"\n}"
---
+++ .next/static/webpack/app/page.fcba3c594da57e1a.hot-update.js
@@ -0,0 +1,55 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./public/images sync recursive ^\\.\\/.*$":
+/*!**************************************!*\
+  !*** ./public/images/ sync ^\.\/.*$ ***!
+  \**************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+var map = {
+	"./MenuImage.png": "(app-pages-browser)/./public/images/MenuImage.png",
+	"./actors/프사_영민.png": "(app-pages-browser)/./public/images/actors/프사_영민.png",
+	"./actors/프사_지은.png": "(app-pages-browser)/./public/images/actors/프사_지은.png",
+	"./actors/프사_혁진.png": "(app-pages-browser)/./public/images/actors/프사_혁진.png",
+	"./mainImage01.jpg": "(app-pages-browser)/./public/images/mainImage01.jpg",
+	"./photos/p1.jpg": "(app-pages-browser)/./public/images/photos/p1.jpg",
+	"./photos/p2.jpg": "(app-pages-browser)/./public/images/photos/p2.jpg",
+	"./photos/p3.jpg": "(app-pages-browser)/./public/images/photos/p3.jpg",
+	"./photos/p4.jpg": "(app-pages-browser)/./public/images/photos/p4.jpg",
+	"./photos/p5.jpg": "(app-pages-browser)/./public/images/photos/p5.jpg",
+	"./placeholder.txt": "(app-pages-browser)/./public/images/placeholder.txt",
+	"./posters/p1.jpg": "(app-pages-browser)/./public/images/posters/p1.jpg",
+	"./어몽어스.png": "(app-pages-browser)/./public/images/어몽어스.png"
+};
+
+
+function webpackContext(req) {
+	var id = webpackContextResolve(req);
+	return __webpack_require__(id);
+}
+function webpackContextResolve(req) {
+	if(!__webpack_require__.o(map, req)) {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	}
+	return map[req];
+}
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = "(app-pages-browser)/./public/images sync recursive ^\\.\\/.*$";
+
+/***/ })
+
+});
