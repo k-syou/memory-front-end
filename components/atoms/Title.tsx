9d932a585c1c5ae9ba799ab35ad46dd76c97ded3
k-syou <rnjstn819@gmail.com>
2024-01-26 15:50:17 +0900

    [update] Title.tsx

--- components/atoms/Title.tsx
+++ components/atoms/Title.tsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import Container from './Container'
-import Text from './Text'
+"use client";
+import React from "react";
+import Container from "./Container";
+import Text from "./Text";
+import { cn } from "@/lib/utils";
 
 type Props = {
-  isUnderLine : boolean
-  text : string
-  pt : string
-  pb : string
-  align? : "right"|"left"|"center"
-}
-const underLineCss = "border-b border-solid border-black box-border"
+  isUnderLine: boolean;
+  text: string;
+  pt: string;
+  pb: string;
+  align?: "right" | "left" | "center";
+};
+const underLineCss = "border-b border-solid border-black box-border";
 
-const Title = ({isUnderLine, text, pt, pb, align}:Props) => {
-  align = align ? align : "left"
+const Title = ({ isUnderLine, text, pt, pb, align }: Props) => {
+  align = align ? align : "left";
+  const commonClassNames = cn(`w-full pt-[${pt}] pb-[${pb}] ${
+    isUnderLine ? underLineCss : ""
+  }`)
   return (
-    <div className={`w-full pt-[${pt}] pb-[${pb}] ${(isUnderLine ? underLineCss : "")}`}>
+    <div
+      className={commonClassNames}
+    >
       <Container variant={"main"}>
-        <Text type="paragraph" variant={"web_h3"} className={`text-${align}`}>{text}</Text>
+        <Text type="paragraph" variant={"web_h3"} className={`text-${align}`}>
+          {text}
+        </Text>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
