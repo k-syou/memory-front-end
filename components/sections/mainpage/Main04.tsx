006d4fdc50da7388236a3d096bbe0977fa869aa5
k-syou <rnjstn819@gmail.com>
2024-01-30 13:09:58 +0900

    [fix] 에러수정

--- components/sections/mainpage/Main04.tsx
+++ components/sections/mainpage/Main04.tsx
@@ -10,7 +10,7 @@ const Main04 = () => {
         isUnderLine
         className="pb-[52px] pt-[198px]"
         text="우리가 만든 기억이에요"
-        align="center"
+        align="text-center"
       />
       <div className="h-auto border-b border-solid border-black w-full box-border">
         <Container variant={"main"} className="h-[752px]">
